9d1b78c202cb11658ff75d6afcb2ea0fac03b661
Aram Tatalyan <atatalyan@krisp.ai>
2023-01-31 01:09:44 +0400

    adding example parameters setting shell scripts.

---
+++ example-set-variables.bat
@@ -0,0 +1,4 @@
+set KRISP_INC=D:\KrispSDK\include
+set KRISP_LIB=D:\KrispSDK\lib
+set LIBSNDFILE_INC=D:\libsndfile\1.2.0\include
+set LIBSNDFILE_LIB=D:\libsndfile\1.2.0\lib
---
+++ example-set-variables.sh
@@ -0,0 +1,4 @@
+export KRISP_INC=$HOME/dev/krisp-audio-sdk-m1-v1/include
+export KRISP_LIB=$HOME/dev/krisp-audio-sdk-m1-v1/lib
+export LIBSNDFILE_INC=/opt/homebrew/Cellar/libsndfile/1.2.0/include
+export LIBSNDFILE_LIB=/opt/homebrew/Cellar/libsndfile/1.2.0/lib
