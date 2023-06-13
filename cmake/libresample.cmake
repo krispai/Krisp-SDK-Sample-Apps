8af5c9e7bf68e2d0302b2a7bfeea71251305e87a
Aram Tatalyan <atatalyan@krisp.ai>
2023-06-13 17:34:10 +0400

    missing commit

---
+++ cmake/libresample.cmake
@@ -0,0 +1,9 @@
+if(NOT DEFINED ENV{LIBRESAMPLE_LIB})
+	message(FATAL_ERROR "LIBRESAMPLE_LIB is not defined")
+endif()
+
+find_library(LIBRESAMPLE_ABSPATH NAMES resample PATHS $ENV{LIBRESAMPLE_LIB})
+if (NOT LIBRESAMPLE_ABSPATH) 
+	message(FATAL_ERROR "Can't find libresample static library")
+endif()
+link_libraries(${LIBRESAMPLE_ABSPATH})
