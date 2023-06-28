2bb62775e60ed13aaff4b2302467943b0c1cc24d
Aram Tatalyan <atatalyan@krisp.ai>
2023-06-28 18:04:30 +0400

    Updating example variables list

--- example-set-variables.bat
+++ example-set-variables.bat
@@ -1,4 +1,6 @@
-set KRISP_INC=D:\KrispSDK\include
-set KRISP_LIB=D:\KrispSDK\lib
+set KRISP_INC=D:\krisp-sdk-builds\7\krisp-audio-sdk-win-v7.0\include
+set KRISP_LIB=D:\krisp-sdk-builds\7\krisp-audio-sdk-win-v7.0\lib\x64\Release
 set LIBSNDFILE_INC=D:\libsndfile\1.2.0\include
 set LIBSNDFILE_LIB=D:\libsndfile\1.2.0\lib
+set LIBRESAMPLE_LIB=D:\krisp-sdk-builds\7\krisp-audio-sdk-win-v7.0\ext_deps\resample\x64\lib
+set MKL_LIB=D:\krisp-sdk-builds\7\krisp-audio-sdk-win-v7.0\ext_deps\mkl\x64\lib
