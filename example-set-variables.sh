ee993155105540e62e4ce627fd4b8d45056e3209
Aram Tatalyan <atatalyan@krisp.ai>
2023-06-28 18:04:30 +0400

    Updating example variables list

--- example-set-variables.sh
+++ example-set-variables.sh
@@ -1,4 +1,5 @@
-export KRISP_INC=$HOME/dev/krisp-audio-sdk-m1-v1/include
-export KRISP_LIB=$HOME/dev/krisp-audio-sdk-m1-v1/lib
-export LIBSNDFILE_INC=/opt/homebrew/Cellar/libsndfile/1.2.0/include
-export LIBSNDFILE_LIB=/opt/homebrew/Cellar/libsndfile/1.2.0/lib
+export KRISP_INC=~/krisp-sdk-builds/7/krisp-audio-sdk-mac-arm-v7.0/include
+export KRISP_LIB=~/krisp-sdk-builds/7/krisp-audio-sdk-mac-arm-v7.0/lib
+export LIBSNDFILE_INC=/opt/homebrew/Cellar/libsndfile/1.2.0_1/include
+export LIBSNDFILE_LIB=/opt/homebrew/Cellar/libsndfile/1.2.0_1/lib
+export LIBRESAMPLE_LIB=~/krisp-sdk-builds/7/krisp-audio-sdk-mac-arm-v7.0/ext_deps/resample/lib
