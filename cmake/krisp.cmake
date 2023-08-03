7e09640a93a2ee91b3a3050604b3ed03b8b1644b
Aram Tatalyan <atatalyan@krisp.ai>
2023-08-04 00:56:32 +0400

    Fixed dependencies.

--- cmake/krisp.cmake
+++ cmake/krisp.cmake
@@ -10,5 +10,5 @@ find_library(LIBKRISP_ABSPATH NAMES krisp-audio-sdk-static PATHS $ENV{KRISP_LIB}
 if (NOT LIBKRISP_ABSPATH) 
 	message(FATAL_ERROR "Can't find krisp-audio-sdk")
 endif()
-include_directories($ENV{KRISP_INC})
-link_libraries(${LIBKRISP_ABSPATH})
+#include_directories($ENV{KRISP_INC})
+#link_libraries(${LIBKRISP_ABSPATH})
