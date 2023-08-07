f0d5cb974518bd718079ff558b35dad9f3d4d98b
Aram Tatalyan <atatalyan@krisp.ai>
2023-08-07 19:43:51 +0400

    Fixing deps.

--- src/sample-dll/dll-test.cpp
+++ src/sample-dll/dll-test.cpp
@@ -1,9 +1,13 @@
+#include <iostream>
 #include <krisp-audio-sdk.hpp>
 #include <krisp-audio-sdk-nc.hpp>
 #include <krisp-audio-sdk-nc-stats.hpp>
 
 
 int main() {
+
+	std::cout << "Hello World" << std::endl;
+
 	krispAudioGlobalInit(nullptr);
 	krispAudioGlobalDestroy();
 	return 0;
