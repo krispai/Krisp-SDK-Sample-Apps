56179060799ba73527bcad7a47933325bc75a8c3
Aram Tatalyan <atatalyan@krisp.ai>
2024-03-28 22:01:47 +0400

    adding configuring script

---
+++ set-vars.sh
@@ -0,0 +1,3 @@
+export LIBSNDFILE_INC=/usr/local/Cellar/libsndfile/1.2.2/include
+export LIBSNDFILE_LIB=/usr/local/Cellar/libsndfile/1.2.2/lib
+export KRISP_SDK_PATH=~/dev/krisp-sdk/krisp-audio-sdk-8.0.0
