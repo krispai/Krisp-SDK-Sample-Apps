0ec6d0332d85a2cb217ffeb6343cf8ff9ec0b202
Aram Tatalyan <atatalyan@krisp.ai>
2024-05-11 01:37:59 +0400

    Added build support for Krisp SDK v8.0.1 for NC package

--- cmake/krisp.cmake
+++ cmake/krisp.cmake
@@ -7,18 +7,26 @@ if (NOT LIBKRISP_EX_ABSPATH)
 	message(FATAL_ERROR "Can't find krisp-audio-sdk-ex in ${KRISP_LIB_DIR}")
 endif()
 
+if (${CMAKE_SYSTEM_PROCESSOR} MATCHES ".*(arm|ARM).*")
+	set (ARM_CPU 1)
+else ()
+	set (ARM_CPU 0)
+endif()
+
 if (WIN32)
 	include(krisp.third.party.win.x64.cmake)
-elseif (APPLE AND ${CMAKE_SYSTEM_PROCESSOR} MATCHES ".*(arm|ARM).*")
+elseif (APPLE AND ARM_CPU)
 	include(krisp.third.party.mac.arm.cmake)
 elseif (APPLE)
 	include(krisp.third.party.mac.x64.cmake)
-elseif (UNIX AND NOT APPLE)
-	message(FATAL_ERROR "not implemented yet")
+elseif (UNIX AND NOT APPLE AND NOT ARM_CPU)
+	include(krisp.third.party.linux.x64.cmake)
+elseif (UNIX AND NOT APPLE AND ARM_CPU)
+	MESSAGE(FATAL_ERROR "NOT IMPLEMENTED YET")
 endif()
 
 set(KRISP_LIBS 
-	${LIBKRISP_ABSPATH}
 	${LIBKRISP_EX_ABSPATH}
+	${LIBKRISP_ABSPATH}
 	${KRISP_THIRDPARTY_LIBS}
 )
--- makefile
+++ makefile
@@ -7,6 +7,16 @@ build: clean
 		-D LIBSNDFILE_LIB=${LIBSNDFILE_LIB}
 	${MAKE} -C build VERBOSE=1
 
+.PHONY: stt
+stt: clean
+	mkdir build
+	cmake -B build -S cmake \
+		-D KRISP_SDK_PATH=${KRISP_SDK_PATH} \
+		-D LIBSNDFILE_INC=${LIBSNDFILE_INC} \
+		-D LIBSNDFILE_LIB=${LIBSNDFILE_LIB} \
+		-D STT=1
+	${MAKE} -C build VERBOSE=1
+
 .PHONY: vs
 vs:
 	mkdir vsbuild
