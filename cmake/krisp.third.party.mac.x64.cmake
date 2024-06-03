aec0d2d8321fe076a92f576d2c41e3972ab7dbf5
Aram Tatalyan <atatalyan@krisp.ai>
2024-06-03 15:31:14 +0400

    fixed mac x64 3rd party deps

--- cmake/krisp.third.party.mac.x64.cmake
+++ cmake/krisp.third.party.mac.x64.cmake
@@ -1,4 +1,4 @@
-set(LIBRESAMPLE_LIB_DIR ${KRISP_SDK_PATH}/ext_deps/resample/lib/x64)
+set(LIBRESAMPLE_LIB_DIR ${KRISP_SDK_PATH}/ext_deps/resample/lib)
 find_library(LIBRESAMPLE_ABSPATH NAMES resample PATHS ${LIBRESAMPLE_LIB_DIR} NO_DEFAULT_PATH)
 if (NOT LIBRESAMPLE_ABSPATH) 
 	message(FATAL_ERROR "Can't find libresample static library in ${LIBRESAMPLE_LIB_DIR}")
